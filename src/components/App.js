84e00404eec0b89a9cb42bdf19e43976cfdd5b4f
David Gibbons <gibbonsdav@gmail.com>
2020-05-31 12:12:04 -0700

    firt commit

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+class App extends React.Component {
+  state = { language: "english" }
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange("english")}
+          />
+          <i
+            className="flag nl"
+            onClick={() => this.onLanguageChange("dutch")}
+          />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
